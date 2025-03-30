4c8b94ffc602abf1ef80a6ac237637e8b3a99fca
Giulia Guasoni <s2103976@bioinfmsc8.bio.ed.ac.uk>
2025-03-30 03:47:15 +0100

    An update

--- backend_python.py
+++ backend_python.py
@@ -1,6 +1,6 @@
 #!/bin/python3
 
-import sys, subprocess, time, os, re, numpy, requests, random, shutil, matplotlib, pandas
+import sys, subprocess, time, os, re, numpy, requests, random, shutil, matplotlib, pandas, argparse
 from Bio import SeqIO
 from Bio import Entrez
 
@@ -8,6 +8,7 @@ from Bio import Entrez
 #1a  Ask the user for the protein and taxonomic group
 protein_family = sys.argv[1]
 taxonomic_group = sys.argv[2]
+
 
 #1b  The necessary information to send the information to NCBI
 Entrez.email = "s2103976@ed.ac.uk"
@@ -19,38 +20,38 @@ search_results = Entrez.read(search_handle)
 search_handle.close()
 
 #1d Using the IDs generated from the first search to obtain fasta files from the protein
-#print(search_results)
 prot_ids = search_results["IdList"]
 if len(prot_ids) < 1:
     print("Sorry, no results were found.")
 
 else:    
-#    print(prot_ids)
     fetch_handle = Entrez.efetch(db="protein", id = prot_ids, rettype="fasta", retmode="text")
     fetched_results = fetch_handle.read()
     fetch_handle.close()
+    
     fetched_file_name = f"{taxonomic_group}_{protein_family}_data.fasta"
-   # if os.path.exists(fetched_file_name):
-       # os.remove(fetched_file_name)
-    #else:    
-     #   with open(fetched_file_name, 'w') as fetched_file:
-      #      fetched_file.write(fetched_results)
     
     with open(fetched_file_name, 'w') as fetched_file:
         fetched_file.write(fetched_results)
-       # fetched_file_name.close()
     print(f"Search results have been written to {fetched_file_name}")
 
 
 #Step 2: Perform analysis on the obtained dataset to determine the level of conservation across the species within that taxonomic group and produce the relevant plots
 #2.a Using clustalo to align the sequences
 aligned_sequences = f"{taxonomic_group}_{protein_family}_alignment.fasta"
-with open(aligned_sequences, 'w') as AS:
-    subprocess.run(f"clustalo -i {fetched_file_name} -o {aligned_sequences} --force",shell=True)
-print(aligned_sequences)
+scores_file = f"{taxonomic_group}_{protein_family}_scores.txt"
+plot_file = f"{taxonomic_group}_{protein_family}_plot.png"
 
-#2.f Message to the user
-print("The basic plan of this site is to perform conservation analysis, and the results are as follows:")
+with open(aligned_sequences, 'w') as AS:
+    subprocess.run(["clustalo", "-i", "fetched_file_name", "-o", "aligned_sequences", "--force"])
+
+subprocess.run(["plotcon", "-sequences", "aligned_sequences", "-scores", "scores_file", "-graph", "png", "-goutfile", "plot_file"])
+
+
+#2.b Message to the user
+print(f"Aligned sequences saved to {aligned_sequences}.")
+print(f"{taxonomic_group}_{protein_family}_output.txt")
+#subprocess.call(f"cat {aligned_sequences}")
 
 #Step 3: Connecting to PROSITE using patmatmotifs
 #3a open the fasta file and create a new file with all the sequences
@@ -66,6 +67,7 @@ with open(f"{fetched_file_name}", "r") as fastalavista:
 #    os.chmod(new_fasta, 0o777)
     print(f"FASTA file created: {os.path.abspath(new_fasta)}")
 
+#Running patmatmotifs to scan the .fasta file
 fasta_to_tsv = f"{taxonomic_group}_{protein_family}_data.tsv"
 #fasta_to_tsv = os.path.join(os.getcwd(), f"{taxonomic_group}_{protein_family}_data.tsv")
 subprocess.run(f"patmatmotifs -sequence {new_fasta} -outfile {fasta_to_tsv} -noprune", shell = True)
